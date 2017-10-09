04788a5a9f91133b94193fb40caff81ebaed8866
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-10-09 19:48:35 -0400

    Final Submission

--- DLLVisualizer.java
+++ DLLVisualizer.java
@@ -4,32 +4,33 @@ import bridges.base.DLelement;
 /**
  * A class that utilizes the BRIDGES library to visualize a doubly-linked list.
  *
- * @author (your name)
+ * @Sam Baskin  
  */
 public class DLLVisualizer
 {
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.Exception
-     */
     public static void main(String[] args) throws Exception{
 
-        // TO DO: Define credentials
-        final String API_KEY = "INSERT_API_KEY_HERE";
-        final String USER_ID = "INSERT_USER_ID_HERE";
+
+        final String API_KEY = "421641178565";
+        final String USER_ID = "samebasket";
         
-        // No need to modify Bridges constructor
         Bridges bridge = new Bridges(1, API_KEY, USER_ID);
 
-        // TO DO: Build the objects to be added to your DLL
-        
 
-        // TO DO: Build the head 
-        DLelement head = null;
+        Track song1 = new Track("Danger Zone", "Kenny Loggins");
+        Track song2 = new Track("Ragu commercial jingle", "Mr. Glaze");
+        Track song3 = new Track("A jar of ragu being opened (10 hour version)", "Ezalg Rm");
+        Track song4 = new Track("The Beautiful Steppe", "Batzorig Vaanchig");
+
+        DLelement head = new DLelement(song1.toString(),song1);
+        DLelement rag1 = new DLelement(song2.toString(),song2);
+        DLelement rag2 = new DLelement(song3.toString(),song3);
+        DLelement mong = new DLelement(song4.toString(),song4);
         
-        // TO DO: Create the rest of the Nodes/DLelements and link them to form a DLL
-        
-        //Prepare for visualization
+        head.setNext(rag1);
+        rag1.setNext(rag2);
+        rag2.setNext(mong);
+
         bridge.setDataStructure(head);
         bridge.setTitle("A Doubly-Linked List");
         bridge.visualize();
